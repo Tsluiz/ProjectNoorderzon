c6b9d53f97be81ce82fe1f8540843d1eca43f985
Gerrit <gerritdegraaff@gmail.com>
2018-06-30 21:31:17 +0200

    tbv verder werken op versie van Rene

--- src/main/java/nl/noorderzon/calculator/Calculator.java
+++ src/main/java/nl/noorderzon/calculator/Calculator.java
@@ -3,13 +3,13 @@ package nl.noorderzon.calculator;
 public class Calculator {
 
     // the static value of the original capacity of the location
-    private int capacity;
+    //private int capacity; --> Via entity klassen data resterende plaatsen ophalen.
     // user input for quantity of reservations
-    private int quantity;
+    private int resQuantity;
     // number of seats left after reservation is made
-    private int available;
-    // number of seats originally available
-    private DB_con cap_loc;
+    //private int available;
+    // number of seats originally available (deze wordt verwijderd duidelijk is hoe we de data uit de DB kunnen halen.
+    private int cap_loc;
 
     /**
      * Create a class which calculates the available seats.
@@ -18,10 +18,9 @@ public class Calculator {
      */
 
     public Calculator(int quant_res) {
-    cap_loc = 500; //deze is voor het testen van de methode. Moet vervangen worden door input DB
-     capacity = cap_loc;
-     quantity = quant_res; //dit moet user-input worden
-     available = capacity - quantity;
+    cap_loc = 397; //deze is voor het testen van de methode. Moet vervangen worden door input DB
+          resQuantity = quant_res; //dit moet user-input worden
+     cap_loc = cap_loc - resQuantity;
 
     }
 
