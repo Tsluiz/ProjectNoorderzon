85672bd0f0e3a58dbeb01fc13d6cf4eb70a50b34
Rene van Rijnswou <vanrijnswou@bgenius.com>
2018-07-01 21:41:32 +0200

    VoorstellingRepository aanpassing v1 namen van html pagina's aangepast naar kleine letters en koppelteken gebruikt

--- src/main/java/nl/noorderzon/repositories/VoorstellingRepository.java
+++ src/main/java/nl/noorderzon/repositories/VoorstellingRepository.java
@@ -1,19 +1,26 @@
 package nl.noorderzon.repositories;
 
-
 import nl.noorderzon.entities.Voorstelling;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
-public interface VoorstellingRepository extends JpaRepository<Voorstelling, Long> {
-
+public interface VoorstellingRepository extends PagingAndSortingRepository<Voorstelling, Integer> {
+    Set<Employee> findByLastName(@Param("name") String name);
 
 	List<Voorstelling> findAll();
 
 }
 
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
+    Set<Employee> findByLastName(@Param("name") String name);
 
+    Employee findFirstByEmail(@Param("email") String email);
+
+    List<Employee> findByJobTitleOrderByHireDate(@Param("job") String job);
+}
