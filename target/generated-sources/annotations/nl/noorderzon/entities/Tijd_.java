5e73ba86c140cc6f55d0af380969acb089194eb8
Rene van Rijnswou <vanrijnswou@bgenius.com>
2018-07-02 22:03:49 +0200

    fix JSON en entities dingen

--- target/generated-sources/annotations/nl/noorderzon/entities/Tijd_.java
+++ target/generated-sources/annotations/nl/noorderzon/entities/Tijd_.java
@@ -1,7 +1,7 @@
 package nl.noorderzon.entities;
 
 import javax.annotation.Generated;
-import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(Tijd.class)
 public abstract class Tijd_ {
 
-	public static volatile ListAttribute<Tijd, Voorstelling> voorstellingPerTijdslot;
+	public static volatile SetAttribute<Tijd, Voorstelling> voorstellingPerTijdslot;
 	public static volatile SingularAttribute<Tijd, String> tijdstip;
 	public static volatile SingularAttribute<Tijd, Integer> id;
 	public static volatile SingularAttribute<Tijd, String> dag;
