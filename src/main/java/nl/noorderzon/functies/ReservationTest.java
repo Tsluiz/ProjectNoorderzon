88c37217d97e608c1886fbf39e0f299d7dc8156c
Gerrit <gerritdegraaff@gmail.com>
2018-07-03 19:12:56 +0200

    First set-up for JunitClass "reservationtest" Deleted waist from reservation class

---
+++ src/main/java/nl/noorderzon/functies/ReservationTest.java
@@ -0,0 +1,21 @@
+package nl.noorderzon.functies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @Test
+    void calculate() {
+        assertEquals();
+    }
+
+    @Test
+    void getCapacity() {
+    }
+
+    @Test
+    void isValidReservation() {
+    }
+}
