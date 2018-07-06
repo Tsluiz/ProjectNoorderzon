3c81fa7d46b48de0be3e3f9f6e131856fcc4c894
Rene van Rijnswou <vanrijnswou@bgenius.com>
2018-07-06 11:40:06 +0200

    reserveren jonguh!

--- src/main/java/nl/noorderzon/functies/ReservationTest.java
+++ src/main/java/nl/noorderzon/functies/ReservationTest.java
@@ -1,24 +1,85 @@
 package nl.noorderzon.functies;
 
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class ReservationTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-    // om de if-else statement te testen worden voor de beide condities een test gemaakt.
+/*The test class ReservationTest*/
+
+public class ReservationTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private Reservation res1;
+    private Reservation res2;
+    private int capacity;
+    private int quant_res;
+
+    /**
+     * Constructor for the test class
+     **/
+
+    /*public ReservationTest(){
+    }*/
+
+    /*
+     * Set up the test reservations
+     * Called before every test method
+     */
+    @Before
+    public void setUp() {
+        res1 = new Reservation(5);
+        res2 = new Reservation(2);
+        capacity = 500;
+        quant_res = 4;
+    }
+
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    /*
+    Tears down the test setup.
+    Method called after every test
+     */
+
+    @After
+    public void tearDown() {
+    }
+
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    /**
+     * Test that a reservation item is correctly initialised (quantity).
+     */
     @Test
-    public void calculateStatementOne() {
-        assertTrue();
+    public void testInit() {
+        Reservation res1 = new Reservation(5);
+        assertEquals(5, res1.getQuant_res());
     }
 
-    private void assertTrue() {
-    }
-
-    @Test
-    void getCapacity() {
-    }
 
     @Test
-    void isValidReservation() {
+    public void out() {
+        System.out.print("ValidReservation");
+        assertEquals("ValidReservation", outContent.toString());
     }
+
+    // als er vrijdag nog tijd is dan deze nog uitwerken
+  /*@Test
+    public void testCalculate1(){
+        Reservation res1 = new Reservation(4);
+         <= capacity;
+        assertEquals(true, )
+    }*/
+
+
+
 }
